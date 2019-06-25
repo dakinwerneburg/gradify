19d37e06c8534629df85e95b668e3ea7fd6b2f75
Eric Hutchins <eric.m.hutchins22@gmail.com>
2019-06-25 11:31:42 -0400

    Formats gradebook page and adds avg calculation
    Formats gradebook overview page to standard Gradify format.  Also
    completes a #TODO to order assignments by due date.  Implements
    average grade calculation.
    
    Closes #39

--- core/views.py
+++ core/views.py
@@ -27,9 +27,10 @@ class StudentSubmissionsView(generic.ListView):
         # TODO use the course currently being viewed
         submissions = StudentSubmission.objects.filter(coursework__course_id=1)
 
-        # Get a list of all course work for this course
-        # TODO sort by dueDate instead
-        coursework = CourseWork.objects.filter(course_id=1).order_by('created')
+        # Get a list of all course work for this course, returns and empty array if none exits
+        coursework = CourseWork.objects.filter(course_id=1).order_by('dueDate')
+        if not coursework:
+            return []
 
         # Get a list of students in the course
         # TODO replace this with a query for the complete class roster
@@ -41,8 +42,8 @@ class StudentSubmissionsView(generic.ListView):
 
         # Initialize the gradebook array with a dictionary element for each student (row).
         # The submissions array is initialized to None for every assignment (column) in this course
-        gradebook = [{'student': s,
-                      'submissions': [None for _ in range(len(coursework))]} for s in students]
+        gradebook = [{'student': s, 'average_grade': None, 'submissions': [None for _ in range(len(coursework))]}
+                     for s in students]
 
         # Populate the gradebook
         for submission in submissions:
@@ -53,6 +54,17 @@ class StudentSubmissionsView(generic.ListView):
             # Enter the submission object into the proper location
             gradebook[row]['submissions'][col] = submission
 
+        total_max = 0
+        for assignment in coursework:
+            total_max += assignment.max_points
+
+        for student in gradebook:
+            total_grade = 0
+            for submission in student['submissions']:
+                if submission:
+                    total_grade += submission.assignedGrade
+            student['average_grade'] = "{:.2%}".format(total_grade / total_max)
+
         return gradebook
 
     def get_context_data(self, **kwargs):
@@ -60,6 +72,7 @@ class StudentSubmissionsView(generic.ListView):
         Augment the context with a list of course work in the correct order
         """
         context = super().get_context_data(**kwargs)
-        context['coursework'] = CourseWork.objects.filter(course_id=1)
+        context['coursework'] = CourseWork.objects.filter(course_id=1).order_by('dueDate')
+        # TODO use the course currently being viewed
         context['course'] = Course.objects.get(pk=1)
         return context
