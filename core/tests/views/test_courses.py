d5fe73194fd8aa71d175d3288357ca827948824d
dakinwerneburg <dakinwerneburg@gmail.com>
2019-06-30 17:29:13 -0400

    Add Assignments to Course Dashboard

--- core/tests/views/test_courses.py
+++ core/tests/views/test_courses.py
@@ -1,11 +1,11 @@
 from django.test import TestCase
 from django.urls import reverse
 
-from core.models import Course
+from core.models import Course, CourseWork
 
 
 class CourseListViewTests(TestCase):
-    fixtures = ['course']
+    fixtures = ['classroom', 'course', 'coursework', 'user']
 
     def test_course_list(self):
         """
@@ -25,3 +25,15 @@ class CourseListViewTests(TestCase):
         response = self.client.get(reverse('course-list'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'No courses found')
+
+    def test_links_work(self):
+        course = Course.objects.get(pk=1)
+        response = self.client.get(reverse('course-detail', kwargs={'pk': 1}))
+        course_link = '<a href="%s">' + course.name + '</a>'
+        self.assertContains(response, course_link % reverse('course-detail', kwargs={'pk': 1}), html=True)
+        self.assertContains(response, '<a href="%s">' % reverse('coursework-detail', kwargs={'pk': 1, 'pk2': 1}))
+
+    def test_all_assignments_listed(self):
+        num_of_assignments = CourseWork.objects.filter(course=1).count()
+        response = self.client.get(reverse('course-detail', kwargs={'pk': 1}))
+        self.assertContains(response, '<tr class="generic-row">', count=num_of_assignments)
