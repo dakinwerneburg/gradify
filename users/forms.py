32fe67392996862dfe41e4344e2c13d6ab394ba2
Eric Hutchins <eric.m.hutchins22@gmail.com>
2019-07-02 18:29:47 -0400

    Formatted login page to Gradify style

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from allauth.account.forms import LoginForm
 from .models import CustomUser
 
 
@@ -12,3 +13,11 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm):
         model = CustomUser
         fields = UserChangeForm.Meta.fields
+
+
+class MyCustomLoginForm(LoginForm):
+
+
+    def login(self, *args, **kwargs):
+        # You must return the original result.
+        return super(MyCustomLoginForm, self).login(*args, **kwargs)
