632b39661743744a3c333bc050057828cd71e8bd
Eric Hutchins <eric.m.hutchins22@gmail.com>
2019-07-02 20:31:27 -0400

    Updates Test folders and adds login form tests
    Adds tests for the login form.  Refactors test directory to include
    form tests.
    
    Closes #2

---
+++ core/tests/forms/test_login_form.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from allauth.account.forms import LoginForm
+
+
+class LoginFormTest(TestCase):
+    def test_blank_entries_fail(self):
+        form_data = {'login': '', 'password': ''}
+        form = LoginForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    def test_username_placeholder_text(self):
+        form = LoginForm()
+        self.assertTrue(form.fields['login'].widget.attrs['placeholder'] == 'Username')
+
+    def test_password_placeholder_text(self):
+        form = LoginForm()
+        self.assertTrue(form.fields['password'].widget.attrs['placeholder'] == 'Password')
