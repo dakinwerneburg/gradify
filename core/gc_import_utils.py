bdbd73c9cd10324a23205315265583764f03956f
ken@lovingoodslcom <ken@lovingoods.com>
2019-07-06 18:17:43 -0400

    Removed duplicate username in get_or_create

--- core/gc_import_utils.py
+++ core/gc_import_utils.py
@@ -118,7 +118,6 @@ def get_or_create_account(student: dict) -> CustomUser:
             'email': profile['emailAddress'],
             'first_name': profile['name']['givenName'],
             'last_name': profile['name']['familyName'],
-            'username': profile['emailAddress'],
         }
 
         student_account = CustomUser(**acct_details)
