4c34240d1ac01387dd283511fb99a67c87a8cc83
Davy L. Marrero <dmarrero3@student.umuc.edu>
2019-07-10 00:39:26 -0400

    Remove insecure config settings

--- gradify/settings/heroku.py
+++ gradify/settings/heroku.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2#del$&afvinwynag1fv%=fa!b9bx35w+^*)5zw5=ymyzn7nu&'
+SECRET_KEY = os.environ.get('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['gradify-app.herokuapp.com']
 
