1e63cd3d4ee8427b2914512b546c0ff3341ed25c
Davy L. Marrero <dmarrero3@student.umuc.edu>
2019-07-13 00:11:47 -0400

    Improves styling of forms
    * Uses existing boostrap classes to improve the look of the forms.
    * Uses the `date` input type for any field that expects a date to
      improve UX.
    * Changes labels to use a more user-friendly field name.
    * Caught a few more breadcrumb boo-boos.

--- core/models.py
+++ core/models.py
@@ -17,12 +17,12 @@ class Course(models.Model):
 
     # Optional fields
     section = models.CharField(max_length=2800, blank=True)
-    descriptionHeading = models.CharField(max_length=3600, blank=True)
+    descriptionHeading = models.CharField(max_length=3600, blank=True, verbose_name="Heading")
     description = models.TextField(max_length=30_000, blank=True)
     room = models.CharField(max_length=650, blank=True)
-    alternateLink = models.CharField(max_length=650, blank=True)
-    startDate = models.DateTimeField(blank=True, null=True)
-    endDate = models.DateTimeField(blank=True, null=True)
+    alternateLink = models.CharField(max_length=650, blank=True, verbose_name="Link")
+    startDate = models.DateTimeField(blank=True, null=True, verbose_name="Start Date")
+    endDate = models.DateTimeField(blank=True, null=True, verbose_name="End Date")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
@@ -68,8 +68,8 @@ class CourseWork(models.Model):
     googleId = models.TextField(max_length=32, blank=True, null=True)
     description = models.TextField(max_length=30_000, blank=True)
     alternateLink = models.TextField(max_length=650, blank=True)
-    maxPoints = models.IntegerField(blank=True)
-    dueDate = models.DateTimeField(blank=True, null=True)
+    maxPoints = models.IntegerField(blank=True, verbose_name="Max Points")
+    dueDate = models.DateTimeField(blank=True, null=True, verbose_name="Due Date")
     creationTime = models.DateTimeField(blank=True, null=True)
     updateTime = models.DateTimeField(blank=True, null=True)
     creatorUserId = models.CharField(max_length=254)
@@ -112,7 +112,8 @@ class CourseWork(models.Model):
     workType = models.CharField(
         max_length=1,
         choices=COURSE_WORK_TYPE_CHOICES,
-        default=UNSPECIFIED
+        default=UNSPECIFIED,
+        verbose_name="Type"
     )
 
     # Course Work Source enum
